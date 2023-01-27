2033931245ead67ac1ee1914d9cf1fca0c09ab09
Micaiah Skolnick <mskolnickdev@gmail.com>
2023-01-27 09:46:10 -0500

    add example usage to `publish` tool

--- tool/publish.dart
+++ tool/publish.dart
@@ -2,6 +2,9 @@ import 'package:process_run/shell.dart';
 
 // Used to quickly publish this package.
 // Soon this will be moved to a github action which runs on [main]
+
+// example usage
+// dart run tool/publish.dart v1.0.4
 Future<void> main(List<String> args) async {
   final newVersion = args.first;
   final results = await Shell().run('dart pub publish -f');
