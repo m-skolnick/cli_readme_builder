9f51b913aefc1d10586cd52e86f17e1dfbc8a423
Micaiah Skolnick <mskolnickdev@gmail.com>
2023-01-27 09:15:32 -0500

    rename tools to tool

--- tool/publish.dart
+++ tool/publish.dart
@@ -4,6 +4,6 @@ import 'package:process_run/shell.dart';
 // Soon this will be moved to a github action which runs on [main]
 Future<void> main(List<String> args) async {
   final newVersion = args.first;
-  await Shell().run('gh release create $newVersion --notes "bugfix release"');
   await Shell().run('dart pub publish');
+  await Shell().run('gh release create $newVersion --notes "bugfix release"');
 }
